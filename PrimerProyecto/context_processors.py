20c9a8e518c76403df21cc88bb8cb151d7a66c1c
Facundo Lemo <facundolemo@gmail.com>
2026-05-20 22:56:27 -0300

    20/05

---
+++ PrimerProyecto/context_processors.py
@@ -0,0 +1,26 @@
+
+from datetime import datetime
+from todolist.models import Etiqueta
+from django.core.cache import cache
+
+def year_context(request):
+    year = datetime.now().year
+    return {"year":year}
+
+def bienvenido_context(request):
+    if request.user.is_authenticated:
+        mensaje = f"Bienvenido {request.user.username}"
+    else:
+        mensaje = "Bienvenido invitado!! Logeate!!"
+    return {"mensaje_bienvenida":mensaje}
+
+def etiquetas_context(request):
+    etiquetas = cache.get("todolist_etiquetas")
+    if etiquetas is None:
+        etiquetas = list(Etiqueta.objects.all())
+        cache.set('todolist_etiquetas', etiquetas, 3600) #1 hs segundos
+        #print("nueva caché seteada.")
+
+    return {"etiquetas_populares":etiquetas}
+
+
